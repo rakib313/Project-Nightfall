13ee0d828272bd0ee3f60da8445a49b4c908a6f7
Rakibul Islam <hridoy.9@gmail.com>
2022-02-26 09:23:51 +0600

    assing Dbexception (try catch) in student and binding data to avoid over posting/mass assignment

--- Nightfall/Nightfall/Models/Student.cs
+++ Nightfall/Nightfall/Models/Student.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Nightfall.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string FirstMidName { get; set; }
+
+        [Required(ErrorMessage = "Please provide Valid for Email field")]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
